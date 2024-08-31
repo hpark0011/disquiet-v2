972df20e06a58c839a68a67db0bce9543a043e06
Hyunsol Park <hpark0011@gmail.com>
2024-08-31 20:41:59 +0900

    finish designing feed tab

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import RightBanners from './components/RightBanners';
 import LeftSidebar from './components/LeftSidebar/LeftSidebar';
 import BackgroundColorSetter from './components/BackgroundColorSetter';
+import FeedTabs from '@/components/FeedTabs';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,6 +11,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className='flex min-h-screen bg-home-feed-bg'>
         <LeftSidebar />
         <div className='flex-grow ml-[72px] transition-all duration-300 ease-in-out'>
+          <FeedTabs />
           <main className='flex-grow'>{children}</main>
         </div>
         <RightBanners />
--- src/components/FeedTabs.tsx
+++ src/components/FeedTabs.tsx
@@ -7,27 +7,47 @@ import Icon from '@/components/Icon';
 
 const FeedTabs = () => {
   return (
-    <div className='flex justify-between items-center p-4 bg-white shadow-sm'>
-      <Tabs defaultValue='전체' className='w-full max-w-[400px]'>
-        <TabsList>
-          <TabsTrigger value='전체'>전체</TabsTrigger>
-          <TabsTrigger value='로그'>로그</TabsTrigger>
-          <TabsTrigger value='프로덕트'>프로덕트</TabsTrigger>
-          <TabsTrigger value='아티클'>아티클</TabsTrigger>
-        </TabsList>
-      </Tabs>
-
-      <div className='flex items-center space-x-4'>
-        <Tabs defaultValue='인기' className='w-[200px]'>
-          <TabsList>
-            <TabsTrigger value='인기'>인기</TabsTrigger>
-            <TabsTrigger value='최신'>최신</TabsTrigger>
+    <div className=' max-w-[680px] mx-auto p-4'>
+      <div className='flex items-center justify-between p-1 border-gray-200 border rounded-xl'>
+        <Tabs defaultValue='전체' className='w-full max-w-[400px] '>
+          <TabsList className='bg-[#EEEEF0] h-fit rounded-lg'>
+            <TabsTrigger className='h-7 rounded-[6px]' value='전체'>
+              전체
+            </TabsTrigger>
+            <div className='w-px h-3 bg-gray-300 mx-0.5' />
+            <TabsTrigger className='h-7 rounded-[6px]' value='로그'>
+              로그
+            </TabsTrigger>
+            <div className='w-px h-3 bg-gray-300 mx-0.5' />
+            <TabsTrigger className='h-7 rounded-[6px]' value='프로덕트'>
+              프로덕트
+            </TabsTrigger>
+            <div className='w-px h-3 bg-gray-300 mx-0.5' />
+            <TabsTrigger className='h-7 rounded-[6px]' value='아티클'>
+              아티클
+            </TabsTrigger>
           </TabsList>
         </Tabs>
 
-        <Button variant='primary' size='sm' className='flex items-center'>
-          <Icon name='plus' className='w-4 h-4 mr-2' />새 글 쓰기
-        </Button>
+        <div className='flex items-center space-x-2'>
+          <div className='flex items-center gap-[6px]'>
+            <Icon name='arrow.up.arrow.down' className='w-4 h-4' />
+            <Tabs defaultValue='인기' className=''>
+              <TabsList className='bg-[#EEEEF0] h-fit rounded-lg'>
+                <TabsTrigger className='h-7 rounded-[6px] px-2' value='인기'>
+                  인기
+                </TabsTrigger>
+                <div className='w-px h-3 bg-gray-300 mx-0.5' />
+                <TabsTrigger className='h-7 rounded-[6px] px-2' value='최신'>
+                  최신
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+          <Button variant='primary' size='sm' className='flex items-center'>
+            <Icon name='plus' className='w-4 h-4 mr-2' />새 글 쓰기
+          </Button>
+        </div>
       </div>
     </div>
   );
